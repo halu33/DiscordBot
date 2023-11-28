98bac69bb86d7ebd2c5decd1a498958f292bfc9b
HALU_33 <gamer.halu.33@gmail.com>
2023-11-29 00:58:51 +0900

    update main.py

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ from config import TOKEN
 from discord.ext import commands
 import logging
 
+
 # Logging設定
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s:%(levelname)s:%(name)s: %(message)s',
                     filename='discordbot.log',
                     filemode='a')
 
+
 # インテントの設定
 intents = discord.Intents.all()
 
 # ボットの設定
-bot = commands.Bot(command_prefix="?", intents=intents)
+prefix = "?"
+bot = commands.Bot(command_prefix=prefix, intents=intents, case_insensitive=True, help_command=None)
 
 #bot起動時の処理 サーバー数を取得しステータスに表示、logにログインを記録
 @bot.event
@@ -28,11 +31,21 @@ async def on_command_error(ctx, error):
     logging.error(f'コマンドエラー発生: {ctx.command} caused by {ctx.author}: {error}')
     await ctx.send(f'エラーが発生しました: {error}')
 
+
 #コマンド設定
+
+#test 挨拶
 @bot.command()
 async def hello(ctx):
     await ctx.send("Hello!")
     logging.info(f'helloコマンド実行: {ctx.author}')
+
+#helpコマンド
+@bot.command()
+async def help(ctx):
+    await ctx.send("https://halu33.net")
+    logging.info(f'helpコマンド実行: {ctx.author}')
+
 
 #bot起動
 bot.run(TOKEN)
