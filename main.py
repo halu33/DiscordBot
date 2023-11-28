7da7dc07a992f9babfa43102d5f804947a714686
HALU_33 <gamer.halu.33@gmail.com>
2023-11-28 23:41:25 +0900

    add files

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+from config import TOKEN
+from discord.ext import commands
+import logging
+
+# Logging設定
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(name)s: %(message)s',
+                    filename='discordbot.log',
+                    filemode='a')
+
+# インテントの設定
+intents = discord.Intents.all()
+
+# ボットの設定
+bot = commands.Bot(command_prefix="?", intents=intents)
+
+#bot起動時の処理 サーバー数を取得しステータスに表示、logにログインを記録
+@bot.event
+async def on_ready():
+    guild_count = len(bot.guilds)
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{guild_count} servers"))
+    logging.info(f'Logged in : {bot.user}')
+
+#エラーをログに記録
+@bot.event
+async def on_command_error(ctx, error):
+    logging.error(f'コマンドエラー発生: {ctx.command} caused by {ctx.author}: {error}')
+    await ctx.send(f'エラーが発生しました: {error}')
+
+#コマンド設定
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
+    logging.info(f'helloコマンド実行: {ctx.author}')
+
+#bot起動
+bot.run(TOKEN)
